b588d9eaf1a0724b7d5c97ed82cadeeed4a54307
Bryan Rengo <bryan.rengo@gmail.com>
2014-01-20 15:16:27 -0600

    initial checkin from paintwithfriends repo with VisualStudio .ignore file

---
+++ trunk/PaintWithFriends/PaintWithFriends/GameHub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace PaintWithFriends
+{
+    public class GameHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public override System.Threading.Tasks.Task OnConnected()
+        {
+            //Hello();
+
+            return Clients.Caller.hello();
+        }
+    }
+}
---
+++ trunk/PaintWithFriends/PaintWithFriends/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+using PaintWithFriends;
+
+[assembly: OwinStartup(typeof(PaintWithFriends.Startup))]
+
+namespace PaintWithFriends
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
