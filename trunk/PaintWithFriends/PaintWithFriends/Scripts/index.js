68839833576dd2bc9758bddb544774aa00586a14
scottmhill <scottmwhill@gmail.com>
2014-01-22 16:03:36 -0600

    added drawing enable/disable

--- trunk/PaintWithFriends/PaintWithFriends/Scripts/index.js
+++ trunk/PaintWithFriends/PaintWithFriends/Scripts/index.js
@@ -3,9 +3,9 @@
 $(function () {
     $('#joinButton').attr('disabled', 'disabled');
 
-    _c.canvas = $("#canvas")[0];//same as document.getElementById
-    _c.ctx = canvas.getContext("2d");
-    _c.last = {};
+    _c.canvasElement = $("#canvas")[0];//same as document.getElementById
+    _c.canvas = canvasElement.getContext("2d");
+    _c.lastPoint = {};
     _c.gameHub = $.connection.gameHub;
     _c.segments = [];
 
@@ -29,20 +29,20 @@ $(function () {
     });
 
     _c.gameHub.client.drawSegment = function (x_from, y_from, x_to, y_to) {
-        _c.ctx.lineWidth = "1.0";
-        _c.ctx.beginPath();
-        _c.ctx.moveTo(x_from, y_from);
-        _c.ctx.lineTo(x_to, y_to);
-        _c.ctx.stroke();
+        _c.canvas.lineWidth = "1.0";
+        _c.canvas.beginPath();
+        _c.canvas.moveTo(x_from, y_from);
+        _c.canvas.lineTo(x_to, y_to);
+        _c.canvas.stroke();
     }
 
     _c.gameHub.client.drawSegments = function (segments) {
         for (var i = 0; i < segments.length; i++) {
-            _c.ctx.lineWidth = "1.0";
-            _c.ctx.beginPath();
-            _c.ctx.moveTo(segments[i].x_from, segments[i].y_from);
-            _c.ctx.lineTo(segments[i].x_to, segments[i].y_to);
-            _c.ctx.stroke();
+            _c.canvas.lineWidth = "1.0";
+            _c.canvas.beginPath();
+            _c.canvas.moveTo(segments[i].x_from, segments[i].y_from);
+            _c.canvas.lineTo(segments[i].x_to, segments[i].y_to);
+            _c.canvas.stroke();
         }
     }
 
@@ -54,46 +54,68 @@ $(function () {
         console.log(s);
     }
 
+    _c.gameHub.client.enableDrawing = function () {
+        _c.canvas.mouseup(function (e) {
+            mouseup_drawaction(e);
+        });
+        _c.canvas.mousedown(function (e) {
+            mousedown_drawaction(e);
+        });
+        _c.canvas.mousemove(function (e) {
+            mousemove_drawaction(e);
+        });
+        $("#clearButton").removeAttr('disabled');
+        _c.canvas.css('cursor', 'crosshair');
+    }
+
+    _c.gameHub.client.disableDrawing = function () {
+        _c.canvas.unbind('mouseup');
+        _c.canvas.unbind('mousedown');
+        _c.canvas.unbind('mousemove');
+        $("#clearButton").attr('disabled', 'disabled');
+        _c.canvas.css('cursor', 'pointer');
+    }
+
     $("#clearButton").click(function () {
         clearCanvas();
         _c.gameHub.server.clear();
     });
 
     function clearCanvas() {
-        _c.ctx.clearRect(0, 0, _c.canvas.width, _c.canvas.height);
+        _c.canvas.clearRect(0, 0, _c.canvas.width, _c.canvas.height);
     }
 
-    $("#canvas").mouseup(function (e) {
+    function mouseup_drawaction(e) {
         if (e.which === 1) _c.mouseclicked = false;
-    });
+    }
 
-    $("#canvas").mousedown(function (e) {
+    function mousedown_drawaction(e) {
         if (e.which === 1) _c.mouseclicked = true;
         var parentOffset = $(this).parent().offset();
-        _c.last.x = (e.pageX - parentOffset.left);
-        _c.last.y = (e.pageY - parentOffset.top);
-    });
+        _c.lastPoint.x = (e.pageX - parentOffset.left);
+        _c.lastPoint.y = (e.pageY - parentOffset.top);
+    }
 
-    $("#canvas").mousemove(function (event) {
+    function mousemove_drawaction(e) {
         if (_c.mouseclicked) {
             var parentOffset = $(this).parent().offset();
-            var rx = (event.pageX - parentOffset.left);
-            var ry = (event.pageY - parentOffset.top);
+            var rx = (e.pageX - parentOffset.left);
+            var ry = (e.pageY - parentOffset.top);
 
-            _c.ctx.lineWidth = "1.0";
-            _c.ctx.beginPath();
-            _c.ctx.moveTo(_c.last.x, _c.last.y);
-            _c.ctx.lineTo(rx, ry);
-            _c.ctx.stroke();
+            _c.canvas.lineWidth = "1.0";
+            _c.canvas.beginPath();
+            _c.canvas.moveTo(_c.lastPoint.x, _c.lastPoint.y);
+            _c.canvas.lineTo(rx, ry);
+            _c.canvas.stroke();
 
-            //_c.gameHub.server.drawSegment(_c.last.x, _c.last.y, rx, ry);
-            pushSegment(_c.last.x, _c.last.y, rx, ry);
-            _c.last.x = rx;
-            _c.last.y = ry;
+            addSegment(_c.lastPoint.x, _c.lastPoint.y, rx, ry);
+
+            _c.lastPoint.x = rx;
+            _c.lastPoint.y = ry;
         }
-    });
+    }
 
-    function pushSegment(x_from, y_from, x_to, y_to) {
+    function addSegment(x_from, y_from, x_to, y_to) {
         var seg = {};
         seg.x_from = x_from;
         seg.y_from = y_from;
