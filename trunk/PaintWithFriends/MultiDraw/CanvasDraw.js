07257f4e42cdfccc6cf83d1edc8a725effa5589e
Bryan Rengo <bryan.rengo@gmail.com>
2014-01-24 11:09:59 -0600

    add MultiDraw project as drawing example for touch events

---
+++ trunk/PaintWithFriends/MultiDraw/CanvasDraw.js
@@ -0,0 +1,231 @@
+﻿function CanvasDraw(target, options) {
+
+    var bgcanvas, bgctx, fgcanvas, fgctx;
+    var darkColorNames = [];
+    var backgroundPolylines = [];
+    var foregroundPolylines = {};
+
+    bgcanvas = document.createElement("canvas");
+    if (typeof bgcanvas.getContext == 'undefined') {
+        var nocanvas = document.createElement("div");
+        nocanvas.style.position = "absolute";
+        nocanvas.style.top = options.promptTop + "px";
+        nocanvas.style.left = options.promptLeft + "px";
+        nocanvas.style.font = options.promptFont;
+        nocanvas.appendChild(document.createTextNode("Canvas drawing is not supported for your browser. Try SVG or VML."));
+        target.appendChild(nocanvas);
+        return;
+    }
+    bgctx = bgcanvas.getContext("2d");
+    bgcanvas.style.position = "absolute";
+    target.appendChild(bgcanvas);
+
+    fgcanvas = document.createElement("canvas");
+    fgctx = fgcanvas.getContext("2d");
+    fgcanvas.style.position = "absolute";
+    target.appendChild(fgcanvas);
+
+    (function () {
+        var darkness = 180;
+        for (var i = 0; i < svgNamedColors.length; ++i) {
+            if (svgNamedColors[i].rgb.r < darkness || svgNamedColors[i].rgb.g < darkness || svgNamedColors[i].rgb.b < darkness)
+                darkColorNames.push(svgNamedColors[i].name);
+        }
+
+        if (options.logMessage)
+            options.logMessage("Drawing using HTML5 canvas with a palette of " + darkColorNames.length + " named colors.");
+    })();
+
+    SetSizeToTarget();
+    PointerDraw(target, BeginPolyline, ExtendPolylineTo, EndPolyline, options.logMessage);
+
+    function DrawCanvasBackground() {
+        bgctx.globalAlpha = 1;
+        bgctx.fillStyle = "White";
+        bgctx.fillRect(0, 0, bgctx.canvas.width, bgctx.canvas.height);
+
+        bgctx.fillStyle = "Black";
+        bgctx.font = options.promptFont;
+        bgctx.fillText(options.prompt, options.promptLeft, options.promptTop + parseFloat(options.promptFont));
+
+        for (var id in backgroundPolylines) {
+            DrawPolyline(bgctx, backgroundPolylines[id]);
+        }
+    }
+
+    function BeginPolyline(wrapper, id, x, y) {
+        if (foregroundPolylines[id.toString()])
+            EndPolyline(wrapper, id);
+
+        foregroundPolylines[id.toString()] = { points: [{ x: x, y: y}], color: darkColorNames[Math.floor(Math.random() * darkColorNames.length)] };
+        RequestRedraw();
+    }
+
+    function ExtendPolylineTo(wrapper, id, x, y) {
+        if (foregroundPolylines[id.toString()])
+            foregroundPolylines[id.toString()].points.push({ x: x, y: y });
+
+        var event = new CustomEvent('move', { "detail": { x: x, y: y, color: foregroundPolylines[id].color } });
+
+        document.dispatchEvent(event);
+
+        RequestRedraw();
+    }
+
+    function EndPolyline(wrapper, id) {
+        var polyline = foregroundPolylines[id.toString()];
+        if (polyline) {
+            var p0 = polyline.points[0];
+            var pLast = polyline.points[polyline.points.length - 1];
+            var distance = Math.sqrt(Math.pow(p0.x - pLast.x, 2) + Math.pow(p0.y - pLast.y, 2));
+            polyline.fill = (distance < options.fillDistance);
+            polyline.ended = true;
+            RequestRedraw();
+        }
+    }
+
+    function SetSizeToTarget() {
+        bgcanvas.width = target.offsetWidth;
+        bgcanvas.height = target.offsetHeight;
+        fgcanvas.width = target.offsetWidth;
+        fgcanvas.height = target.offsetHeight;
+        DrawCanvasBackground();
+    }
+
+    this.SetSizeToTarget = function () { SetSizeToTarget(); };
+
+    this.ClearPicture = function () {
+        backgroundPolylines.length = 0;
+        DrawCanvasBackground();
+    };
+
+    var redrawPending = false;
+
+    function RequestRedraw() {
+
+        function RedrawForeground() {
+            redrawPending = false;
+
+            fgctx.clearRect(0, 0, fgctx.canvas.width, fgctx.canvas.height);
+
+            for (var id in foregroundPolylines) {
+                DrawPolyline(foregroundPolylines[id].ended ? bgctx : fgctx, foregroundPolylines[id]);
+
+                if (foregroundPolylines[id].ended) {
+                    backgroundPolylines.push(foregroundPolylines[id]);
+                    delete foregroundPolylines[id.toString()];
+                }
+            }
+        }
+
+        if (!redrawPending) {
+            redrawPending = true;
+
+            if (window.msRequestAnimationFrame)
+                window.msRequestAnimationFrame(RedrawForeground);
+            else if (window.webkitRequestAnimationFrame)
+                window.webkitRequestAnimationFrame(RedrawForeground);
+            else
+                window.setTimeout(RedrawForeground, Math.floor(1000 / 60));
+        }
+    }
+
+    function DrawPolyline(ctx, polyline) {
+        if (polyline.points.length == 1) {
+            ctx.beginPath();
+            ctx.arc(polyline.points[0].x, polyline.points[0].y, 5, 0, Math.PI, true);
+            ctx.arc(polyline.points[0].x, polyline.points[0].y, 5, Math.PI, Math.PI * 2, true);
+            ctx.fillStyle = polyline.color;
+            ctx.globalAlpha = 0.5;
+
+            ctx.fill();
+        }
+        else if (polyline.points.length > 1) {
+            ctx.beginPath();
+            ctx.moveTo(polyline.points[0].x, polyline.points[0].y);
+
+            for (var i = 1; i < polyline.points.length; ++i) {
+                ctx.lineTo(polyline.points[i].x, polyline.points[i].y);
+            }
+
+            ctx.lineCap = "round";
+            ctx.lineJoin = "round";
+            ctx.lineWidth = 10;
+            ctx.strokeStyle = polyline.color;
+            ctx.globalAlpha = 0.5;
+            ctx.stroke();
+
+            if (polyline.fill) {
+                ctx.fillStyle = polyline.color;
+                ctx.globalAlpha = 0.3;
+                ctx.fill();
+            }
+        }
+    }
+}
+
+/// Polyfill CustomEvent for IE9+10
+(function () {
+    function CustomEvent(event, params) {
+        params = params || { bubbles: false, cancelable: false, detail: undefined };
+        var evt = document.createEvent('CustomEvent');
+        evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+        return evt;
+    };
+
+    CustomEvent.prototype = window.CustomEvent.prototype;
+
+    window.CustomEvent = CustomEvent;
+})();
+
+
+//Copyright (c) 2010 Nicholas C. Zakas. All rights reserved.
+//MIT License
+function EventTarget() {
+    this._listeners = {};
+}
+
+EventTarget.prototype = {
+
+    constructor: EventTarget,
+
+    addListener: function (type, listener) {
+        if (typeof this._listeners[type] == "undefined") {
+            this._listeners[type] = [];
+        }
+
+        this._listeners[type].push(listener);
+    },
+
+    fire: function (event) {
+        if (typeof event == "string") {
+            event = { type: event };
+        }
+        if (!event.target) {
+            event.target = this;
+        }
+
+        if (!event.type) {  //falsy
+            throw new Error("Event object missing 'type' property.");
+        }
+
+        if (this._listeners[event.type] instanceof Array) {
+            var listeners = this._listeners[event.type];
+            for (var i = 0, len = listeners.length; i < len; i++) {
+                listeners[i].call(this, event);
+            }
+        }
+    },
+
+    removeListener: function (type, listener) {
+        if (this._listeners[type] instanceof Array) {
+            var listeners = this._listeners[type];
+            for (var i = 0, len = listeners.length; i < len; i++) {
+                if (listeners[i] === listener) {
+                    listeners.splice(i, 1);
+                    break;
+                }
+            }
+        }
+    }
+};
