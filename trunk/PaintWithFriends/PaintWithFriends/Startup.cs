d241fa9dc58899fcbe778b63ae5c7137bd05eb48
Bryan Rengo <bryan.rengo@gmail.com>
2014-04-02 10:52:23 -0500

    new project with multi provider signon (fb, google, microsoft)

--- trunk/PaintWithFriends/PaintWithFriends/Startup.cs
+++ trunk/PaintWithFriends/PaintWithFriends/Startup.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Owin;
 using Owin;
 using PaintWithFriends;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
 
 [assembly: OwinStartup(typeof(PaintWithFriends.Startup))]
 
@@ -8,11 +11,19 @@ namespace PaintWithFriends
 {
     public class Startup
     {
-
-        // add a comment to deploy update to azure
         public void Configuration(IAppBuilder app)
         {
             app.MapSignalR();
+
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions() { AuthenticationMode = AuthenticationMode.Active });
+
+            app.UseFacebookAuthentication("420314314779503", "3fafa1fdea8070567a8d8154d33057b3");
+
+            app.UseGoogleAuthentication("1023576817075-4kf8ikvr0s1n7avvf2sv1gs9jm3tqv6k.apps.googleusercontent.com", "5yWzMUVx3CUIHIkSDfVjMxea");
+            // javascript origin: http://pwf.azurewebsites.net/  redirect URI's: http://pwf.azurewebsites.net/oauth2callback
+
+            app.UseMicrosoftAccountAuthentication("0000000044114D67", "-sbktxOj9P3bK7-SsiOxChNxjqRXGMqn");
         }
     }
 }
