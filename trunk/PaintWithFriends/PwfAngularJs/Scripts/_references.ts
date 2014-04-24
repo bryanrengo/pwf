a67ac28204b6d39589f80479b7187e4284e07549
Bryan Rengo <bryan.rengo@gmail.com>
2014-04-24 15:30:25 -0500

    cleanup branch

--- trunk/PaintWithFriends/PwfAngularJs/Scripts/_references.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="angular-cookies.d.ts"/>
-/// <reference path="angular-resource.d.ts"/>
-/// <reference path="angular-route.d.ts"/>
-/// <reference path="angular-cookies.d.ts"/>
-/// <reference path="angular-sanitize.d.ts"/>
-/// <reference path="angular.d.ts"/>
-/// <reference path="jquery.d.ts"/>
