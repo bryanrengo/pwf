7f6f2ee162d0b6a5741198903c89a393e4a9c091
Bryan Rengo <bryan.rengo@gmail.com>
2014-04-30 10:20:24 -0500

    igamestate was not added appropriately

---
+++ branches/angularjspwf/PwfAngularJs/Hubs/Models/IGameState.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace App.Hubs.Models
+{
+    public interface IGameState
+    {
+        IGroupManager Groups { get; set; }
+        Game GetGame(Player player);
+        Player GetPlayer(string playerConnectionId);
+        Player CreatePlayer(string playerName, string connectionId);
+        IEnumerable<Player> GetAllPlayers();
+        IEnumerable<Game> GetAllGames();        
+        void RemoveGame(Game game);
+        void RemovePlayer(Player player);
+    }
+}
