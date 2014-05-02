c31557b207df4206f4c39fd5fe25e31dd8cbcc8c
Bryan Rengo <bryan.rengo@gmail.com>
2014-05-02 15:23:05 -0500

    logincontroller was missing....

---
+++ branches/angularjspwf/PwfAngularJs/Api/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace App.Api
+{
+    public class LoginController : ApiController
+    {
+        [Authorize]
+        public bool Index()
+        {
+            return false;
+        }
+    }
+}
